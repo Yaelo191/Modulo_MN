f3945ece68eee25dd9c935120596435f4e1668c7
Yaelo191 <ceyagog@gmail.com>
2024-03-03 03:19:12 -0600

    update

--- Soluciones_de_ecuaciones_de _una_variable/secante_v2.py
+++ Soluciones_de_ecuaciones_de _una_variable/secante_v2.py
@@ -1,9 +1,7 @@
-import numpy as np
-
 def funcion(x):
     return eval(def_funcion)
 
-def secante_v2(P0=1, P1=10, tolerancia=0.000001, max_iter=10000):
+def metodo_secante_v2(P0=1, P1=10, tolerancia=0.000001, max_iter=10000):
     iteracion = 2  
     q0 = funcion(P0)
     q1 = funcion(P1)
@@ -21,4 +19,4 @@ def secante_v2(P0=1, P1=10, tolerancia=0.000001, max_iter=10000):
 def_funcion = input("Ingrese una función de x (por ejemplo 'x**2 + 7*x + 12'): ")
 d0 = float(input("Ingrese una semilla: "))
 d1 = float(input("Ingrese una segunda semilla: "))
-res = secante_v2(P0=d0, P1=d1)
+res = metodo_secante_v2(P0=d0, P1=d1)
