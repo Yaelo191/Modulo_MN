414488d69dcd1298ff51b846772e876922e37dc1
Yaelo191 <ceyagog@gmail.com>
2024-03-03 03:17:38 -0600

    update

--- Soluciones_de_ecuaciones_de _una_variable/newton.py
+++ Soluciones_de_ecuaciones_de _una_variable/newton.py
@@ -1,18 +1,21 @@
 import numpy as np
 
 def funcion(x):
-    return eval(def_funcion)  # Utiliza eval para evaluar la expresión de la función
-
-def punto_fijo(P0=1, tolerancia=0.000001, max_iter=10000):
-    iteracion = 0  # Iniciar desde la iteración 0
+    return eval(def_funcion) 
+def funcion_prime(x):
+    return eval(def_funcion_prime)
+    
+def metodo_newton(P0=1, tolerancia=0.000001, max_iter=10000):
+    iteracion = 1  
     for i in range(max_iter):
-        x = funcion(P0)
+        x = P0 - funcion(P0)/funcion_prime(P0)
         if (abs(x - P0) < tolerancia):
             print(f"Raíz encontrada: {x}")
             return
         P0 = x 
     print(f"Se alcanzó el número máximo de iteraciones ({max_iter}). La aproximación actual es: {x}")
     return 
-def_funcion = input("Ingrese una función de x despejando x(por ejemplo para 'x**2 + 7*x + 12'-->'-(x**2 + 12)/7'): ")
+def_funcion = input("Ingrese una función de x (por ejemplo 'x**2 + 7*x + 12': ")
+def_funcion_prime = input("Ingrese la derivada de la funcion anterior (por ejemplo para '2*x +7': ")
 d0 = float(input("Ingrese una semilla: "))
-res = punto_fijo(P0=d0)
+res = metodo_newton(P0=d0)
--- Soluciones_de_ecuaciones_de _una_variable/secante.py
+++ Soluciones_de_ecuaciones_de _una_variable/secante.py
@@ -1,21 +1,24 @@
 import numpy as np
 
 def funcion(x):
-    return eval(def_funcion) 
-def funcion_prime(x):
-    return eval(def_funcion_prime)
-    
-def metodo_newton(P0=1, tolerancia=0.000001, max_iter=10000):
-    iteracion = 1  
+    return eval(def_funcion)
+
+def metodo_secante(P0=1, P1=10, tolerancia=0.000001, max_iter=10000):
+    iteracion = 2  
+    q0 = funcion(P0)
+    q1 = funcion(P1)
     for i in range(max_iter):
-        x = P0 - funcion(P0)/funcion_prime(P0)
-        if (abs(x - P0) < tolerancia):
+        x = P1 -q1*(P1 - P0)/(q1 - q0)
+        if (abs(x - P1) < tolerancia):
             print(f"Raíz encontrada: {x}")
             return
-        P0 = x 
+        P0 = P1
+        q0 = q1
+        P1 = x
+        q1 = funcion(x)
     print(f"Se alcanzó el número máximo de iteraciones ({max_iter}). La aproximación actual es: {x}")
     return 
-def_funcion = input("Ingrese una función de x (por ejemplo 'x**2 + 7*x + 12': ")
-def_funcion_prime = input("Ingrese la derivada de la funcion anterior (por ejemplo para '2*x +7': ")
+def_funcion = input("Ingrese una función de x (por ejemplo 'x**2 + 7*x + 12'): ")
 d0 = float(input("Ingrese una semilla: "))
-res = metodo_newton(P0=d0)
+d1 = float(input("Ingrese una segunda semilla: "))
+res = metodo_secante(P0=d0, P1=d1)
