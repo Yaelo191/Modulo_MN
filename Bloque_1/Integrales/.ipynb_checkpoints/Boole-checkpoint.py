6bd6f8b0a748f8c2219e46971b747c6bc204569a
Yaelo191 <ceyagog@gmail.com>
2024-03-27 10:39:17 -0600

    update

--- Bloque_1/Integrales/.ipynb_checkpoints/Boole-checkpoint.py
+++ Bloque_1/Integrales/.ipynb_checkpoints/Boole-checkpoint.py
@@ -3,36 +3,27 @@
     
 #a = 0.
 #b = 1.
-#Npuntos=52
-
-def integracion_boole(f, a , b , Npuntos=100):
+def integracion_boole(f, a , b , Npuntos=1000000):
 
     if not Npuntos % 4 == 0:
         corrector = Npuntos % 4
         Npuntos -= corrector
         print(f"Se requiere un número de puntos múltiplo de 4, los puntos ahora son {Npuntos}")
-    Npuntos += 1
+
     h = (b-a)/(Npuntos-1)
-    Resultado = 0
+    Resultado = 0.0
     
     for i in range(1, Npuntos+1):
         x = a + (i-1) * h
         if i == 1 or i == Npuntos:
             contribucion = f(x)*(14/45)*h
-            #print(f'14/45 {contribucion}')
         elif (i-1) % 4 == 0:
             contribucion = f(x)*(28/45)*h  
-            #print(f'28/45 {contribucion}')
         elif (i+1) % 4 == 0:
             contribucion = f(x)*(24/45)*h
-            #print(f'24/45 {contribucion}')
         else:
             contribucion = f(x)*(64/45)*h
-            #print(f'64/45 {contribucion}')
         Resultado += contribucion
-    print(f"La integral da como resultado: ")
-    print(Resultado)
-    return
-    
+    return Resultado
     
 #integracion_boole(f, a , b)
